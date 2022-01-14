9790a538074d2c689425a1027dbeaa7c58c821c6
Motti789 <mchillywilly@gmail.com>
2022-01-14 10:59:41 -0500

    commit tree e9c0c39ba8219b15fcd622ccaeb17df49540f5fe

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post "/posts", to: "posts#create"
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
-  patch "/posts/:id", to: "posts#update
+  patch "/posts/:id", to: "posts#update"
 end
